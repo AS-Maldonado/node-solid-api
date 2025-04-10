9331216e17abc715d6524d606d800bc0681a7ecd
Augusto Maldonado <augusto.maldonado@openk.com.br>
2025-04-10 18:51:51 -0300

    feat: iniciando repository

--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,31 +1,27 @@
 import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma.users.repository";
 import { hash } from "bcryptjs";
 
 interface RegisterServiceRequest {
-  name: string;
-  email: string;
-  password: string;
+    name: string;
+    email: string;
+    password: string;
 }
 
 export async function RegisterService({
-  name,
-  email,
-  password,
+    name,
+    email,
+    password,
 }: RegisterServiceRequest) {
-  const password_hash = await hash(password, 6);
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
+    const password_hash = await hash(password, 6);
+    const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+            email,
+        },
+    });
 
-  if (userWithSameEmail) throw new Error("Email already exists!");
+    if (userWithSameEmail) throw new Error("Email already exists!");
 
-  await prisma.user.create({
-    data: {
-      name,
-      email,
-      password_hash,
-    },
-  });
+    const prismaUsersRepository = new PrismaUsersRepository();
+    await prismaUsersRepository.create({ name, email, password_hash });
 }
